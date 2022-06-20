c88e37ddc2dee1e82acb6e58b162a59fb0b63453
ana-pairo <ana.pairo2@gmail.com>
2022-06-20 15:09:47 -0300

    VersãoFinal c/ Relógio

--- scripts.js
+++ scripts.js
@@ -5,8 +5,18 @@ function comparador() {
 let contador = 0;
 let ganhou = false;
 let numeroDeCartas = 0;
+let contando = 0;
+let relogio = 0;
+let comecou = false;
 
 comecar();
+
+function tempo (){
+    relogio = setInterval(function(){
+        contando++;
+        document.querySelector(".cronometro").innerHTML = `${contando}s`;
+    } ,1000)
+}
 
 function comecar(){
 
@@ -35,16 +45,19 @@ function comecar(){
 }
 
  function clicada(elementoDiv) {
+    if (comecou == false){
+        tempo();
+        comecou = true;
+    }
 
     if (elementoDiv.classList.contains("virada")==false && elementoDiv.classList.contains("acerto")== false && elementoDiv.classList.contains("primeira") == false && document.querySelector(".segunda")==null){
-
-        contador ++;
 
         elementoDiv.classList.add("virada")
         let gif = elementoDiv.querySelector(".back");
         let parrotFront = elementoDiv.querySelector(".front");
         parrotFront.classList.add("hidden")
         gif.classList.add("appear");
+        contador ++;
       
         if (document.querySelector(".primeira") == null){
             elementoDiv.classList.add("primeira");
@@ -71,19 +84,26 @@ function comecar(){
 
             ganhou = document.querySelectorAll(".virada").length == numeroDeCartas;
 
-            setTimeout(function(){
-                if(ganhou){
-                    alert(`Você ganhou em ${contador} jogadas!`);
+            if (ganhou) {
+                setTimeout(function(){
+                    clearInterval(relogio);
+                    alert(`Você ganhou em ${contador} jogadas e em ${contando} segundos!`);
                     let resposta = prompt("Gostaria de reiniciar a partida?");
 
                     while(resposta !== "sim" && resposta !== "não") {
                         resposta = prompt("Gostaria de reiniciar a partida? Por favor, digite 'sim' ou 'não'.");
                     }
+
                     if(resposta == "sim"){
+                        contando = 0;
+                        contador =0;
+                        comecou = false;
+                        document.querySelector(".cronometro").innerHTML = "0s";
                         comecar();
                     }
-                }
-            }, 1700)
+                }, 2000)
+            }            
+
         }   
     }
 }
